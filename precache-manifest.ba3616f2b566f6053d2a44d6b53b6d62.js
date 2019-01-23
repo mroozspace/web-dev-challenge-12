e940b2a1f4865647150d16dfeddf37cbf0596e80
adrian mroz <a.mrooz93@gmail.com>
2019-01-23 10:46:31 +0100

    Updates

---
+++ precache-manifest.ba3616f2b566f6053d2a44d6b53b6d62.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ddbe3ae28f852f991305e990948f040",
+    "url": "/webdev12/static/media/5.8ddbe3ae.jpg"
+  },
+  {
+    "revision": "52cb51d002a2eee96f38",
+    "url": "/webdev12/static/css/main.5d2bbbfe.chunk.css"
+  },
+  {
+    "revision": "bb55ad18942154118df2",
+    "url": "/webdev12/static/js/1.bb55ad18.chunk.js"
+  },
+  {
+    "revision": "a6dce8c822b7a9e7fa7f",
+    "url": "/webdev12/static/js/runtime~main.a6dce8c8.js"
+  },
+  {
+    "revision": "8615a40e233389c81cfec0eca89b35b2",
+    "url": "/webdev12/static/media/homeBg.8615a40e.jpg"
+  },
+  {
+    "revision": "467ae5f2493cb31af19e3d6c4b7af960",
+    "url": "/webdev12/static/media/1.467ae5f2.jpg"
+  },
+  {
+    "revision": "ce72e9f7e84ca9f45ee559ce04a44a7f",
+    "url": "/webdev12/static/media/2.ce72e9f7.jpg"
+  },
+  {
+    "revision": "162a2bca9c1e6ca137b3b84373af6d47",
+    "url": "/webdev12/static/media/3.162a2bca.jpg"
+  },
+  {
+    "revision": "73f61e63987a787c2e0f3980edbf721d",
+    "url": "/webdev12/static/media/4.73f61e63.jpg"
+  },
+  {
+    "revision": "52cb51d002a2eee96f38",
+    "url": "/webdev12/static/js/main.52cb51d0.chunk.js"
+  },
+  {
+    "revision": "740ef296da92292947a75188a14cde14",
+    "url": "/webdev12/static/media/6.740ef296.jpg"
+  },
+  {
+    "revision": "3762054da16b7543659149fae7c9c615",
+    "url": "/webdev12/static/media/logoColored.3762054d.png"
+  },
+  {
+    "revision": "5702edfb2cfd9b55bd7d73d6d41c3428",
+    "url": "/webdev12/static/media/homeBg1.5702edfb.jpg"
+  },
+  {
+    "revision": "526f1542557c0b885ab702aa394da0b1",
+    "url": "/webdev12/static/media/homeBg2.526f1542.jpg"
+  },
+  {
+    "revision": "7646e7ea95ed269b638157a938ff53ec",
+    "url": "/webdev12/static/media/homeBg3.7646e7ea.jpg"
+  },
+  {
+    "revision": "d44204b8b5acfcb450e6d443a71f81be",
+    "url": "/webdev12/static/media/menuBg.d44204b8.jpg"
+  },
+  {
+    "revision": "8d568870c8ee6b267788e074181db14c",
+    "url": "/webdev12/static/media/7.8d568870.jpg"
+  },
+  {
+    "revision": "5a71619223f9bd73cd98da7ee94a7d8d",
+    "url": "/webdev12/index.html"
+  }
+];
